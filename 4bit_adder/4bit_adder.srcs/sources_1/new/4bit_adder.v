1531473e02f3850abbe5e3d6c03c35bbb9625893
PAWAN A KESTWAL <101412114+Pawankestwal@users.noreply.github.com>
2023-05-28 20:36:51 +0530

    Add files via upload

---
+++ 4bit_adder/4bit_adder.srcs/sources_1/new/4bit_adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.04.2023 22:47:39
+// Design Name: 
+// Module Name: 4bit_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 4bit_adder(
+
+    );
+endmodule
---
+++ Counter_Project/Counter_Project.srcs/sources_1/new/Display_Seven_Segment.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.04.2023 11:05:48
+// Design Name: 
+// Module Name: Display_Seven_Segment
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Display_Seven_Segment(
+    input [3:0] digit,
+    output [5:0] seven_seg
+    );
+endmodule
